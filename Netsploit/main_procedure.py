e9fc6657611235e47e4ba3e88ef3e433e4f1ebc3
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-05-29 16:51:51 +0200

    commentato secondo if

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -116,11 +116,13 @@ def main_procedure (attacker_ip, config_file, stealth=False, stealth_sleep=0):
 
             """if(attack_name=="tomcat_server" and C.TARGETS_DOCKERS[target_ip][0]["docker_name"]!="tomcat_server"):
                 print(f"{C.COL_YELLOW}[*] Special attack tomcat_server cannot be done on this machine, skipping... {C.COL_RESET}")
-                continue"""
+                continue
+            """  
+            """                              
             if(attack_name=="smtp_server" and C.TARGETS_DOCKERS[target_ip][0]["docker_name"]!="smtp_server"):
                 print(f"{C.COL_RED}[-] Special attack smtp_server cannot be done on this machine, skipping... {C.COL_RESET}")
                 continue
-            
+            """ 
             
             if(attack_type=="ResourceAttack"):
                 attack_obj=ResourceAttack(attack_name,attack_instr,attack_wait, mc)
