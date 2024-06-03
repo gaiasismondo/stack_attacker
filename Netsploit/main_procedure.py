16516470a9708c39d1baf6f320ef6f3cfb16ce10
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-06-03 10:48:04 +0200

    prova ripistino vecchio attack_db

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -138,7 +138,7 @@ def main_procedure (attacker_ip, config_file, stealth=False, stealth_sleep=0):
                 attack_obj=Metasploit_Attack(attack_name,attack_instr,attack_wait, mc)
                 
             
-            attack_obj = attack_db.create_attack(ra, target_ip, attacker_ip, LPORT)
+            #attack_obj = attack_db.create_attack(ra, target_ip, attacker_ip, LPORT)
 
             if(type(attack_obj)==SshAttack and OOBSession==None):
                 print(f"{C.COL_RED}[-] can't use OOB attacks without an established session!{C.COL_RESET}")
