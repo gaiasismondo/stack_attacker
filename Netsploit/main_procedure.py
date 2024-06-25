177b11ebf5bdd04f7615840add8d51af6d4c5ab9
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-06-25 11:03:00 +0200

    permesso attacco smtp su macchina tomcat

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -89,7 +89,7 @@ def main_procedure (attacker_ip, config_file, stealth=False, stealth_sleep=0):
                     break
 
             print(f"{C.COL_GREEN}[+] attacking ({target_ip}) with {attack_name}{C.COL_RESET}")
-            """
+            
             if(attack_name=="tomcat_server" and C.TARGETS_DOCKERS[target_ip][0]["docker_name"]!="tomcat_server"):
                 print(f"{C.COL_YELLOW}[*] Special attack tomcat_server cannot be done on this machine, skipping... {C.COL_RESET}")
                 continue
@@ -98,7 +98,7 @@ def main_procedure (attacker_ip, config_file, stealth=False, stealth_sleep=0):
             if(attack_name=="smtp_server" and C.TARGETS_DOCKERS[target_ip][0]["docker_name"]!="smtp_server"):
                 print(f"{C.COL_RED}[-] Special attack smtp_server cannot be done on this machine, skipping... {C.COL_RESET}")
                 continue
-            
+            """
 
             attack_obj= attack_db.create_attack(ra, target_ip, attacker_ip, LPORT)
 
