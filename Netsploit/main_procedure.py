d8e091783a58e13de7952d0b254f810ca1f14566
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-06-27 11:45:28 +0200

    prova modifica di come viene scelta LPORT nel main_procedure

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -79,15 +79,16 @@ def main_procedure (attacker_ip, config_file, stealth=False, stealth_sleep=0):
         
         for ra in randomized_attack:
 
-            LPORT = C.DEFAULT_LPORT  #PENTESTER LISTENING PORT
+            #LPORT = C.DEFAULT_LPORT  #PENTESTER LISTENING PORT
             
             attack_name=attack_db.attack_dict[ra].attack
-
+            LPORT = C.TARGETS_DOCKERS[target_ip]["exposed_port"]
+            """
             for p in C.TARGETS_DOCKERS[target_ip]:
                 if(attack_name in p["attack_list"]):
                     LPORT=p["exposed_port"]
                     break
-
+            """
             print(f"{C.COL_GREEN}[+] attacking ({target_ip}) with {attack_name}{C.COL_RESET}")
             
             """
