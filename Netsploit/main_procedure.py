619fe1d839bc42f966ddd8acaeda7b80e7e13989
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-10-03 12:53:08 +0200

    corretto errore

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -196,3 +196,20 @@ def main_procedure(attacker_ip, config_file, attack_sequence_file=None, stealth=
                     sleep(stealth_sleep)
 
     print(f"{C.COL_GREEN} Attack complete!! {C.COL_RESET}")
+
+
+#Da config.json viene recuperato l'ip dell'attaccante e lo si passa al main insieme a config.json
+#Come terzo parametro si può passare un file json contenente la sequenza degli attacchi da eseguire
+#Se non viene passato gli attacchi vengono effettuati in ordine casuale fino a quando uno non va a buon fine
+if(__name__=='__main__'):
+   mode = -1
+   while (mode!=0 and mode!=1):
+       mode = int(input("\nHow do you want to execute the synthetic attacker?\n0 : with random attack sequence\n1 : with attack sequence read from json file\nPress 0 or 1 :   "))
+       if(mode!=0 and mode!=1):
+           print("Invalid choice, press 0 or 1")
+   if(mode==0):
+       print("Attacking with random attack sequence")
+       main_procedure(C.ATTACKER_VM,"config.json")
+   else:
+       print("Reading attack sequence from Attack_sequence.json and attacking with that")
+       main_procedure(C.ATTACKER_VM,"config.json", "Attack_sequence.json")
