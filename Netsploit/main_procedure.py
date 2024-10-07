b594a7c666c353de75494bd8e1ed70aa25b7a83e
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-10-07 15:30:05 +0200

    rinominato directory attacco bruteForce

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -207,7 +207,7 @@ def main_procedure(attacker_ip, config_file, attack_sequence_file=None, stealth=
                     print(f"{C.COL_YELLOW}[*] sleeping {stealth_sleep} seconds to make the attack stealthier...{C.COL_RESET}")
                     sleep(stealth_sleep)
 
-            print(f"{C.COL_GREEN}[+] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -{C.COL_RESET}")
+            print(f"{C.COL_GREEN}- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -{C.COL_RESET}")
 
          
 
