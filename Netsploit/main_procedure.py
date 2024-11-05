53e1295228f17052a628586a7837b668cc1523f2
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-11-05 10:01:11 +0100

    corretto errore indentazione

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -192,13 +192,13 @@ def main_procedure(attacker_ip, config_file, attack_sequence_file=None, stealth=
 #Se non viene passato gli attacchi vengono effettuati in ordine casuale fino a quando uno non va a buon fine
 if(__name__=='__main__'):
    mode = -1
-   while (mode!=0 and mode!=1 and mode!=2):
+    while (mode!=0 and mode!=1 and mode!=2):
        mode = int(input("\nHow do you want to execute the synthetic attacker?\n[0] : with random attack sequence\n[1] : with attack sequence read from json file\nPress 0 or 1 :   "))
-       if(mode!=0 and mode!=1):
+        if(mode!=0 and mode!=1):
            print("Invalid choice, press 0 or 1")
-   if(mode==0):
-       main_procedure(C.ATTACKER_VM,"config.json")
-   elif(mode==1):
-       main_procedure(C.ATTACKER_VM,"config.json", "Attack_sequence.json")
+    if(mode==0):
+        main_procedure(C.ATTACKER_VM,"config.json")
+    elif(mode==1):
+        main_procedure(C.ATTACKER_VM,"config.json", "Attack_sequence.json")
     else:
         main_procedure(C.ATTACKER_VM,"config.json", "new_attack_sequence.json")
