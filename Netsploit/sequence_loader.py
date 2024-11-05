9f3a0c8cfb645fa0a15c99c071cae4d7e5ac2328
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-11-05 10:23:10 +0100

    corretto errore di stampa

--- Netsploit/sequence_loader.py
+++ Netsploit/sequence_loader.py
@@ -2,23 +2,18 @@ import json
 
 #legge la sequenza di attacchi dal file preso in input
 def load_attack_sequence(file_path):
-    try:
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-            attack_sequence = data.get("attack_sequence", [])
-            return attack_sequence
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print(f"Errore nel caricamento del file di sequenza di attacco: {e}")
-        return []
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return [(attack["attack_name"], attack["ip"], attack["other_attribute"]) for attack in data["attack_sequence"]]
+
 
 
 #stampa in modo ordinato il contenuto del file attack sequence
 def print_attack_sequence(file_path):
     try:
         attack_sequence = load_attack_sequence(file_path)
-        
-        for i, (attack_name, ip, generic_attribute) in enumerate(attack_sequence, start=1):
-            print(f"STEP {i}:   attack_name: {attack_name}  IP: {ip}  ther_attribute: {generic_attribute if generic_attribute else 'N/A'}\n")
+        for i, (attack_name, ip, other_attribute) in enumerate(attack_sequence, start=1):
+            print(f"STEP {i}:   attack_name: {attack_name}  IP: {ip}  other_attribute: {other_attribute if other_attribute else 'N/A'}\n")
         
     except FileNotFoundError:
         print(f"Errore: il file '{file_path}' non è stato trovato.")
