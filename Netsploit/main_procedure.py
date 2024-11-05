b5240ccd0c432171fe95e506c1d68b61b87582f9
vboxuser <vboxuser@ubuntu2204.myguest.virtualbox.org>
2024-11-05 10:13:32 +0100

    lettura e stampa attack sequence file

--- Netsploit/main_procedure.py
+++ Netsploit/main_procedure.py
@@ -105,8 +105,12 @@ def main_procedure(attacker_ip, config_file, attack_sequence_file=None, stealth=
             print(f"{C.COL_GREEN}- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -{C.COL_RESET}")
 
     elif(attack_sequence_file == "new_attack_sequence.json"):
-        sequenza_prova = sequence_loader.load_attack_sequence("new_attack_sequence.json")
-        print(sequenza_prova)
+        print(f"{C.COL_YELLOW}\nReading attack sequence from Attack_sequence.json and attacking with that")
+        print(f"{C.COL_YELLOW}ATTACK SEQUENCE:")
+        prova_sequenza = sequence_loader.load_attack_sequence(attack_sequence_file)
+        sequence_loader.print_attack_sequence(attack_sequence_file)
+
+        
     
 
     #CASO 1: PROCEDURA CON ORDINE LETTO DA FILE
@@ -194,7 +198,7 @@ if(__name__=='__main__'):
     mode = -1
     while (mode!=0 and mode!=1 and mode!=2):
         mode = int(input("\nHow do you want to execute the synthetic attacker?\n[0] : with random attack sequence\n[1] : with attack sequence read from json file\nPress 0 or 1 :   "))
-        if(mode!=0 and mode!=1):
+        if(mode!=0 and mode!=1 and mode!=2):
             print("Invalid choice, press 0 or 1")
     if(mode==0):
         main_procedure(C.ATTACKER_VM,"config.json")
--- Netsploit/sequence_loader.py
+++ Netsploit/sequence_loader.py
@@ -10,3 +10,22 @@ def load_attack_sequence(file_path):
     except (FileNotFoundError, json.JSONDecodeError) as e:
         print(f"Errore nel caricamento del file di sequenza di attacco: {e}")
         return []
+
+
+#stampa in modo ordinato il contenuto del file attack sequence
+def print_attack_sequence(file_path):
+    try:
+        attack_sequence = load_attack_sequence(file_path)
+        
+        print("\nATTACK SEQUENCE: ")
+        for i, (attack_name, ip, generic_attribute) in enumerate(attack_sequence, start=1):
+            print(f"Step {i}:")
+            print(f"  Attack name: {attack_name}")
+            print(f"  IP: {ip}")
+            print(f" Other_attribute: {generic_attribute if generic_attribute else 'N/A'}")
+          
+    except FileNotFoundError:
+        print(f"Errore: il file '{file_path}' non è stato trovato.")
+
+
+
